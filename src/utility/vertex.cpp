95f1b2d1815618cf3c8e355f32b56f60cf405cd6
Brendan Lewis <brendanelewis@gmail.com>
2021-07-10 23:05:00 -0400

    Fix: properly check for NaN (forgot this file)

--- src/utility/vertex.cpp
+++ src/utility/vertex.cpp
@@ -1,20 +1,30 @@
 #include "vertex.h"
+#include <cmath>
 
-vertex2::vertex2(float a, float b) {
-    this->u = a;
-    this->v = b;
+vertex::vertex(float a, float b) {
+    this->x = a;
+    this->y = b;
+    this->z = NAN;
 }
 
-int vertex2::getSizeOf() const {
-    return (2 * sizeof(float));
-}
-
-vertex3::vertex3(float a, float b, float c) {
+vertex::vertex(float a, float b, float c) {
     this->x = a;
     this->y = b;
     this->z = c;
 }
 
-int vertex3::getSizeOf() const {
-    return (3 * sizeof(float));
+int vertex::getSizeOf() const {
+    if (std::isnan(this->z)) {
+        return (2 * sizeof(float));
+    } else {
+        return (3 * sizeof(float));
+    }
+}
+
+bool vertex::is2D() const {
+    return std::isnan(this->z);
+}
+
+bool vertex::is3D() const {
+    return !std::isnan(this->z);
 }
