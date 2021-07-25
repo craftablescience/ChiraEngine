0e11ba491423affe948a068e94bc41c55922ed5f
Brendan Lewis <brendanelewis@gmail.com>
2021-07-24 22:42:23 -0400

    Chore: cleanup vertex structs

--- src/utility/vertex.cpp
+++ src/utility/vertex.cpp
@@ -6,7 +6,7 @@ position::position(GLfloat x, GLfloat y, GLfloat z) {
     this->z = z;
 }
 
-bool operator==(const position &pos1, const position &pos2) {
+bool operator==(const position& pos1, const position& pos2) {
     return pos1.x == pos2.x && pos1.y == pos2.y && pos1.z == pos2.z;
 }
 
@@ -16,7 +16,7 @@ normal::normal(GLfloat r, GLfloat g, GLfloat b) {
     this->b = b;
 }
 
-bool operator==(const normal &norm1, const normal &norm2) {
+bool operator==(const normal& norm1, const normal& norm2) {
     return norm1.r == norm2.r && norm1.g == norm2.g && norm1.b == norm2.b;
 }
 
@@ -26,8 +26,8 @@ color::color(GLfloat r, GLfloat g, GLfloat b) {
     this->b = b;
 }
 
-bool operator==(const color &color1, const color &color2) {
-    return color1.r == color2.r && color1.g == color2.g && color1.b == color2.b;
+bool operator==(const color& col1, const color& col2) {
+    return col1.r == col2.r && col1.g == col2.g && col1.b == col2.b;
 }
 
 uv::uv(GLfloat u, GLfloat v) {
@@ -35,7 +35,7 @@ uv::uv(GLfloat u, GLfloat v) {
     this->v = v;
 }
 
-bool operator==(const uv &uv1, const uv &uv2) {
+bool operator==(const uv& uv1, const uv& uv2) {
     return uv1.u == uv2.u && uv1.v == uv2.v;
 }
 
@@ -73,6 +73,6 @@ vertex::vertex(const struct position& newPos, const struct normal& newNorm, cons
 
 vertex::vertex() : pos(), norm(), col(), uvMap() {}
 
-bool operator==(const vertex &v1, const vertex &v2) {
+bool operator==(const vertex& v1, const vertex& v2) {
     return v1.pos == v2.pos && v1.norm == v2.norm && v1.col == v2.col && v1.uvMap == v2.uvMap;
 }
--- src/utility/vertex.h
+++ src/utility/vertex.h
@@ -5,26 +5,30 @@
 struct position {
     GLfloat x, y, z;
     explicit position(GLfloat x = 0, GLfloat y = 0, GLfloat z = 0);
-    friend bool operator==(const position& pos1, const position& pos2);
 };
+
+bool operator==(const position& pos1, const position& pos2);
 
 struct normal {
     GLfloat r, g, b;
     explicit normal(GLfloat r = 0, GLfloat g = 0, GLfloat b = 0);
-    friend bool operator==(const normal& pos1, const normal& pos2);
 };
+
+bool operator==(const normal& norm1, const normal& norm2);
 
 struct color {
     GLfloat r, g, b;
     explicit color(GLfloat r = 1, GLfloat g = 1, GLfloat b = 1);
-    friend bool operator==(const color& pos1, const color& pos2);
 };
+
+bool operator==(const color& col1, const color& col2);
 
 struct uv {
     GLfloat u, v;
     explicit uv(GLfloat u = 0, GLfloat v = 0);
-    friend bool operator==(const uv& uv1, const uv& uv2);
 };
+
+bool operator==(const uv& uv1, const uv& uv2);
 
 struct vertex {
     position pos;
@@ -37,7 +41,8 @@ struct vertex {
     vertex(GLfloat x, GLfloat y, GLfloat z, GLfloat r, GLfloat g, GLfloat b);
     vertex(GLfloat x, GLfloat y, GLfloat z, GLfloat u, GLfloat v);
     vertex(GLfloat x, GLfloat y, GLfloat z);
-    explicit vertex(const struct position& pos, const struct normal& norm, const struct color& col, const struct uv& tex);
+    vertex(const struct position& pos, const struct normal& norm, const struct color& col, const struct uv& tex);
     vertex();
-    friend bool operator==(const vertex& v1, const vertex& v2);
 };
+
+bool operator==(const vertex& v1, const vertex& v2);
