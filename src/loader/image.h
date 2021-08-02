33eaa036a791adbfefb482fec8980fe08af27798
Brendan Lewis <brendanelewis@gmail.com>
2021-08-02 14:42:42 -0400

    Chore: make image getData() method overrideable

--- src/loader/image.h
+++ src/loader/image.h
@@ -6,7 +6,7 @@ class image {
 public:
     image(const std::string& filepath, int* width, int* height, int* fileChannels, int desiredChannels, bool vflip = true);
     virtual ~image();
-    [[nodiscard]] unsigned char* getData();
+    [[nodiscard]] virtual unsigned char* getData();
 protected:
     unsigned char* data;
 };
