90064f7f67502a7e7a0104cb7d1fe7f47c10ded1
Brendan Lewis <brendanelewis@gmail.com>
2021-08-01 20:17:54 -0400

    Fix: unlitMaterial should not be abstract

--- src/render/unlitMaterial.h
+++ src/render/unlitMaterial.h
@@ -6,4 +6,7 @@ class unlitMaterial : public abstractMaterial {
     void use() override {
         this->shaderName->use();
     }
+    void updateLighting(const std::vector<std::unique_ptr<abstractLight>>& lights) override {
+        // Unlit lol
+    }
 };
