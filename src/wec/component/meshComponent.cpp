54b4f861fa61d6836c98de857b93b9d430c1c32d
Brendan Lewis <brendanelewis@gmail.com>
2021-08-25 00:14:00 -0400

    WIP: add rotationComponent

--- src/wec/component/meshComponent.cpp
+++ src/wec/component/meshComponent.cpp
@@ -1,6 +1,6 @@
 #include "meshComponent.h"
 
-meshComponent::meshComponent(mesh* mesh_, const glm::vec3& pos) : positionComponent(pos) {
+meshComponent::meshComponent(mesh* mesh_, const glm::vec3& pos, const glm::vec3& rot) : positionComponent(pos), rotationComponent(rot) {
     this->meshPtr = mesh_;
 }
 
@@ -8,6 +8,10 @@ void meshComponent::render(double delta) {
     if (this->positionChanged) {
         // todo: set mesh position
         this->positionChanged = false;
+    }
+    if (this->rotationChanged) {
+        // todo: set mesh rotation
+        this->rotationChanged = false;
     }
     this->meshPtr->render();
 }
--- src/wec/component/meshComponent.h
+++ src/wec/component/meshComponent.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include "positionComponent.h"
+#include "rotationComponent.h"
 #include "../../render/mesh.h"
 
-class meshComponent : public positionComponent {
+class meshComponent : public positionComponent, public rotationComponent {
 public:
-    explicit meshComponent(mesh* mesh_, const glm::vec3& pos);
+    explicit meshComponent(mesh* mesh_, const glm::vec3& pos, const glm::vec3& rot);
     void render(double delta) override;
     void stop() override;
 private:
