025c7134022a6caae21cc99792ac98b026350099
Brendan L <brendanelewis@gmail.com>
2021-10-05 01:52:23 -0400

    Fix: uuidGenerator compiles

--- src/utility/uuidGenerator.h
+++ src/utility/uuidGenerator.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "uuid.h"
+#include <memory>
 
 namespace chira {
     class uuidGenerator {
@@ -9,23 +10,22 @@ namespace chira {
             if (!uuidGenerator::isInitialized) {
                 uuidGenerator::initializeGenerator();
             }
-            return uuidGenerator::generator();
+            return uuidGenerator::generator->operator()();
         }
         static std::string getNewUUIDString() {
             return uuids::to_string(uuidGenerator::getNewUUID());
         }
     private:
         static inline bool isInitialized = false;
-        static std::mt19937 engine;
-        static uuids::uuid_random_generator generator;
+        static inline std::unique_ptr<uuids::uuid_random_generator> generator = nullptr;
 
         static void initializeGenerator() {
             std::random_device rd;
             auto seedData = std::array<int, std::mt19937::state_size>{};
             std::generate(std::begin(seedData), std::end(seedData), std::ref(rd));
             std::seed_seq seq(std::begin(seedData), std::end(seedData));
-            uuidGenerator::engine = std::mt19937{seq};
-            uuidGenerator::generator = uuids::uuid_random_generator{uuidGenerator::engine};
+            std::mt19937 mtEngine = std::mt19937{seq};
+            uuidGenerator::generator = std::make_unique<uuids::uuid_random_generator>(mtEngine);
             uuidGenerator::isInitialized = true;
         }
     };
