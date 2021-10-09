ab53864939d1906580d8cac56496d80a1a25b088
Brendan L <brendanelewis@gmail.com>
2021-10-09 00:32:56 -0400

    Fix: game runs on linux again

---
+++ src/utility/uuidGenerator.cpp
@@ -0,0 +1,24 @@
+#include "uuidGenerator.h"
+
+using namespace chira;
+
+uuidGenerator::uuidGeneratorInstance::uuidGeneratorInstance() {
+    std::generate(std::begin(this->seedData), std::end(this->seedData), std::ref(this->rd));
+    std::seed_seq seq(std::begin(this->seedData), std::end(this->seedData));
+    this->generator = std::make_unique<std::mt19937>(seq);
+}
+
+[[nodiscard]] std::mt19937* uuidGenerator::uuidGeneratorInstance::getGenerator() const {
+    return this->generator.get();
+}
+
+uuids::uuid uuidGenerator::getNewUUID() {
+    if (!uuidGenerator::generator) {
+        uuidGenerator::generator = std::make_unique<uuidGeneratorInstance>();
+    }
+    return uuids::uuid_random_generator{*uuidGenerator::generator->getGenerator()}();
+}
+
+std::string uuidGenerator::getNewUUIDString() {
+    return uuids::to_string(uuidGenerator::getNewUUID());
+}
--- src/utility/uuidGenerator.h
+++ src/utility/uuidGenerator.h
@@ -5,28 +5,20 @@
 
 namespace chira {
     class uuidGenerator {
-    public:
-        static uuids::uuid getNewUUID() {
-            if (!uuidGenerator::isInitialized) {
-                uuidGenerator::initializeGenerator();
-            }
-            return uuidGenerator::generator->operator()();
-        }
-        static std::string getNewUUIDString() {
-            return uuids::to_string(uuidGenerator::getNewUUID());
-        }
     private:
-        static inline bool isInitialized = false;
-        static inline std::unique_ptr<uuids::uuid_random_generator> generator = nullptr;
-
-        static void initializeGenerator() {
-            std::random_device rd;
-            auto seedData = std::array<int, std::mt19937::state_size>{};
-            std::generate(std::begin(seedData), std::end(seedData), std::ref(rd));
-            std::seed_seq seq(std::begin(seedData), std::end(seedData));
-            std::mt19937 mtEngine = std::mt19937{seq};
-            uuidGenerator::generator = std::make_unique<uuids::uuid_random_generator>(mtEngine);
-            uuidGenerator::isInitialized = true;
-        }
+        class uuidGeneratorInstance {
+        public:
+            uuidGeneratorInstance();
+            [[nodiscard]] std::mt19937* getGenerator() const;
+        private:
+            std::random_device rd{};
+            std::array<int, std::mt19937::state_size> seedData{};
+            std::unique_ptr<std::mt19937> generator;
+        };
+    public:
+        static uuids::uuid getNewUUID();
+        static std::string getNewUUIDString();
+    private:
+        static inline std::unique_ptr<uuidGeneratorInstance> generator;
     };
 }
