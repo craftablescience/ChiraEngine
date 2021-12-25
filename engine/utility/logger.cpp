43d46ea17c1756f9aa3150c80ccf8b4dca51af63
Brendan <brendanelewis@gmail.com>
2021-12-25 17:31:44 -0500

    fix: flush stdout on every log event

--- engine/utility/logger.cpp
+++ engine/utility/logger.cpp
@@ -8,20 +8,22 @@ std::unordered_map<uuids::uuid, std::function<void(const loggerType&,const std::
 
 void logger::log(const loggerType& type, const std::string& source, const std::string& message) {
     switch (type) {
+        // Using std::endl here instead of '\n'
+        // sometimes with the latter, the buffer doesn't get flushed until the application exits
         case INFO:
-            std::cout << LOGGER_INFO_PREFIX << "[" << source << "] " << message << '\n';
+            std::cout << LOGGER_INFO_PREFIX << "[" << source << "] " << message << std::endl;
             break;
         case INFO_IMPORTANT:
-            std::cout << "\x1B[32m" << LOGGER_INFO_IMPORTANT_PREFIX << "[" << source << "] " << message << "\033[0m" << '\n';
+            std::cout << "\x1B[32m" << LOGGER_INFO_IMPORTANT_PREFIX << "[" << source << "] " << message << "\033[0m" << std::endl;
             break;
         case OUTPUT:
-            std::cout << "\x1B[34m" << LOGGER_OUTPUT_PREFIX << "[" << source << "] " << message << "\033[0m" << '\n';
+            std::cout << "\x1B[34m" << LOGGER_OUTPUT_PREFIX << "[" << source << "] " << message << "\033[0m" << std::endl;
             break;
         case WARN:
-            std::cout << "\x1B[33m" << LOGGER_WARNING_PREFIX << "[" << source << "] " << message << "\033[0m" << '\n';
+            std::cout << "\x1B[33m" << LOGGER_WARNING_PREFIX << "[" << source << "] " << message << "\033[0m" << std::endl;
             break;
         case ERR:
-            std::cout << "\x1B[31m" << LOGGER_ERROR_PREFIX << "[" << source << "] " << message << "\033[0m" << '\n';
+            std::cout << "\x1B[31m" << LOGGER_ERROR_PREFIX << "[" << source << "] " << message << "\033[0m" << std::endl;
             break;
     }
     logger::runLogHooks(type, source, message);
