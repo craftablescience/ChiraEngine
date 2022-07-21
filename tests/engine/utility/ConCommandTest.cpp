80053d23ba8e4119c5713b059107cae811b21d95
craftablescience <brendanelewis@gmail.com>
2022-07-21 00:21:21 -0400

    feat: add tests for concommands

---
+++ tests/engine/utility/ConCommandTest.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include <utility/ConCommand.h>
+
+using namespace chira;
+
+TEST(ConCommand, createConCommand) {
+    {
+        ConCommand my_concommand{"my_concommand", [](const std::vector<std::string> &) {}};
+        ASSERT_TRUE(ConCommandRegistry::hasConCommand("my_concommand"));
+    }
+    ASSERT_FALSE(ConCommandRegistry::hasConCommand("my_concommand"));
+}
+
+TEST(ConCommand, callbackCallsBack) {
+    bool check = false;
+    ConCommand my_concommand{"my_concommand", [&check](const std::vector<std::string>&) {
+        check = true;
+    }};
+    my_concommand.fire({});
+    ASSERT_TRUE(check);
+}
+
+TEST(ConCommand, callbackGetsArgs) {
+    std::string check1, check2;
+    ConCommand my_concommand{"my_concommand", [&check1, &check2](const std::vector<std::string>& args) {
+        ASSERT_EQ(args.size(), 2);
+        check1 = args[0];
+        check2 = args[1];
+    }};
+    my_concommand.fire({"hello", "world"});
+    ASSERT_STREQ(check1.c_str(), "hello");
+    ASSERT_STREQ(check2.c_str(), "world");
+}
