a1e13ef86d96ecce426ab718b44cebf9616bc7c3
craftablescience <brendanelewis@gmail.com>
2022-07-31 19:41:09 -0400

    feat: add optional setter and getter member functions to serializable properties

--- engine/resource/Properties.h
+++ engine/resource/Properties.h
@@ -12,19 +12,25 @@ namespace chira::Serialize {
 
 template<class Class, typename T>
 struct Property {
-    constexpr Property(T Class::*member_, std::string_view name_)
+    constexpr Property(T Class::*member_, std::string_view name_, T(Class::*memberFuncGetter_)(), void(Class::*memberFuncSetter_)(T))
             : member(member_)
-            , name(name_) {}
+            , name(name_)
+            , memberFuncGetter(memberFuncGetter_)
+            , memberFuncSetter(memberFuncSetter_) {}
 
     using Type = T;
 
     T Class::*member;
     std::string_view name;
+    T(Class::*memberFuncGetter)();
+    void(Class::*memberFuncSetter)(T);
 };
 
+// https://stackoverflow.com/questions/20405569/in-function-call-why-doesnt-nullptr-match-a-pointer-to-a-template-object
+// what the fuck C++
 template<class Class, typename T>
-constexpr auto createProperty(T Class::*member, std::string_view name) {
-    return Property<Class, T>{member, name};
+constexpr auto createProperty(T Class::*member, std::string_view name, T(Class::*memberFuncGetter)() = static_cast<T(Class::*)()>(nullptr), void(Class::*memberFuncSetter)(T) = static_cast<void(Class::*)(T)>(nullptr)) {
+    return Property<Class, T>{member, name, memberFuncGetter, memberFuncSetter};
 }
 
 template<class C>
@@ -34,19 +40,28 @@ void fromJSON(C* obj, const nlohmann::json& data) {
         const auto property = std::get<i>(C::props);
         using PropType = typename decltype(property)::Type;
         if (data.contains(property.name)) {
-            obj->*(property.member) = data.at(property.name).template get<PropType>();
+            const auto value = data.at(property.name).template get<PropType>();
+            if (property.memberFuncSetter) {
+                (obj->*(property.memberFuncSetter))(value);
+            } else {
+                obj->*(property.member) = value;
+            }
         }
     });
 }
 
 template<class C>
-nlohmann::json toJSON(const C& obj) {
+nlohmann::json toJSON(C& obj) {
     nlohmann::json out;
 
     const auto numProperties = std::tuple_size<decltype(C::props)>::value;
     forSequence(std::make_index_sequence<numProperties>{}, [&](auto i) {
         const auto property = std::get<i>(C::props);
-        out[property.name] = obj.*(property.member);
+        if (property.memberFuncGetter) {
+            out[property.name] = (obj.*(property.memberFuncGetter))();
+        } else {
+            out[property.name] = obj.*(property.member);
+        }
     });
 
     return out;
@@ -57,3 +72,9 @@ nlohmann::json toJSON(const C& obj) {
 #define CHIRA_PROPS static inline const auto props = std::make_tuple
 #define CHIRA_PROP(clazz, member) chira::Serialize::createProperty(&clazz::member, #member)
 #define CHIRA_PROP_NAMED(clazz, member, name) chira::Serialize::createProperty(&clazz::member, #name)
+#define CHIRA_PROP_GET(clazz, member, getter) chira::Serialize::createProperty(&clazz::member, #member, &clazz::getter)
+#define CHIRA_PROP_NAMED_GET(clazz, member, name, getter) chira::Serialize::createProperty(&clazz::member, #name, &clazz::getter)
+#define CHIRA_PROP_SET(clazz, member, setter) chira::Serialize::createProperty(&clazz::member, #member, static_cast<decltype(member)(clazz::*)()>(nullptr), &clazz::setter)
+#define CHIRA_PROP_NAMED_SET(clazz, member, name, setter) chira::Serialize::createProperty(&clazz::member, #name, static_cast<decltype(member)(clazz::*)()>(nullptr), &clazz::setter)
+#define CHIRA_PROP_GET_SET(clazz, member, getter, setter) chira::Serialize::createProperty(&clazz::member, #member, &clazz::getter, &clazz::setter)
+#define CHIRA_PROP_NAMED_GET_SET(clazz, member, name, getter, setter) chira::Serialize::createProperty(&clazz::member, #name, &clazz::getter, &clazz::setter)
--- tests/engine/resource/Properties.cpp
+++ tests/engine/resource/Properties.cpp
@@ -21,19 +21,6 @@ struct SimpleStructWithProps {
     );
 };
 
-struct ComplexStructWithProps {
-    std::vector<int> list{1, 2, 5, 6};
-    std::unordered_map<std::string,bool> dict{ // python 👍
-            {"first", true},
-            {"second", false}
-    };
-
-    CHIRA_PROPS (
-            CHIRA_PROP(ComplexStructWithProps, list),
-            CHIRA_PROP(ComplexStructWithProps, dict)
-    );
-};
-
 TEST(Properties, simpleDeserialize) {
     SimpleStructWithProps test{};
     Serialize::fromJSON(&test, nlohmann::json::parse(R"({
@@ -62,6 +49,19 @@ TEST(Properties, simpleSerialize) {
     EXPECT_STREQ(testJson["name"].get<std::string>().c_str(), "hello");
     EXPECT_TRUE(testJson["b"]);
 }
+
+struct ComplexStructWithProps {
+    std::vector<int> list{1, 2, 5, 6};
+    std::unordered_map<std::string,bool> dict{ // python 👍
+            {"first", true},
+            {"second", false}
+    };
+
+    CHIRA_PROPS (
+            CHIRA_PROP(ComplexStructWithProps, list),
+            CHIRA_PROP(ComplexStructWithProps, dict)
+    );
+};
 
 TEST(Properties, complexDeserialize) {
     ComplexStructWithProps test{};
@@ -96,4 +96,61 @@ TEST(Properties, complexSerialize) {
     ASSERT_EQ(testJson["dict"].size(), 2);
     EXPECT_TRUE(testJson["dict"]["first"]);
     EXPECT_FALSE(testJson["dict"]["second"]);
+}
+
+struct StructWithGettersAndSettersForProps {
+    int a = 4;
+    bool b = true;
+    std::string c{"hello"};
+
+    bool aWasGot = false;
+    int getA() {
+        this->aWasGot = true;
+        return this->a + 1;
+    }
+
+    bool bWasSet = false;
+    void setB(bool b_) {
+        this->b = !b_;
+        this->bWasSet = true;
+    }
+
+    bool cWasGot = false, cWasSet = false;
+    std::string getC() {
+        this->cWasGot = true;
+        return this->c + " getter";
+    }
+    void setC(std::string c_) {
+        this->c = std::move(c_);
+        this->c += " setter";
+        this->cWasSet = true;
+    }
+
+    CHIRA_PROPS (
+            CHIRA_PROP_GET(StructWithGettersAndSettersForProps, a, getA),
+            CHIRA_PROP_SET(StructWithGettersAndSettersForProps, b, setB),
+            CHIRA_PROP_GET_SET(StructWithGettersAndSettersForProps, c, getC, setC)
+    );
+};
+
+TEST(Properties, gettersAndSetters) {
+    StructWithGettersAndSettersForProps test1{};
+    nlohmann::json testJson = Serialize::toJSON(test1);
+    EXPECT_TRUE(test1.aWasGot);
+    EXPECT_EQ(testJson["a"], 5);
+    EXPECT_TRUE(testJson["b"]);
+    EXPECT_TRUE(test1.cWasGot);
+    EXPECT_STREQ(testJson["c"].get<std::string>().c_str(), "hello getter");
+
+    StructWithGettersAndSettersForProps test2{};
+    Serialize::fromJSON(&test2, nlohmann::json::parse(R"({
+        "a": 2,
+        "b": true,
+        "c": "inside"
+    })"));
+    EXPECT_EQ(test2.a, 2);
+    EXPECT_TRUE(test2.bWasSet);
+    EXPECT_FALSE(test2.b);
+    EXPECT_TRUE(test2.cWasSet);
+    EXPECT_STREQ(test2.c.c_str(), "inside setter");
 }
