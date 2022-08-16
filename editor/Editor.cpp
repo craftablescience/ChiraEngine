3bc39a797f9c484d2d7d078375ffbf1e310fb1c7
ash <anticream39@yahoo.com>
2022-08-16 13:50:21 -0400

    add new color scheme and hide main editor panel

--- editor/Editor.cpp
+++ editor/Editor.cpp
@@ -60,7 +60,9 @@ static inline void addResourceFolderSelected() {
 class MainEditorPanel : public IPanel {
 public:
     MainEditorPanel() : IPanel(TR("ui.MainEditor.title"), true) {
-        this->flags |= ImGuiWindowFlags_NoBackground;
+        this->flags |= ImGuiWindowFlags_NoBackground |
+                        ImGuiWindowFlags_NoInputs   |
+                        ImGuiWindowFlags_NoDecoration;
     }
 
     // Opens a file dialog used to select a model definition
@@ -180,6 +182,34 @@ int main(int argc, const char* const argv[]) {
 #endif
 
     Engine::init();
+
+    // ImGui Color Setting
+    ImVec4* colors = ImGui::GetStyle().Colors;
+    colors[ImGuiCol_FrameBg]                = ImVec4(0.16f, 0.09f, 0.37f, 0.54f);
+    colors[ImGuiCol_FrameBgHovered]         = ImVec4(0.62f, 0.42f, 0.65f, 1.00f);
+    colors[ImGuiCol_FrameBgActive]          = ImVec4(0.27f, 0.27f, 0.76f, 1.00f);
+    colors[ImGuiCol_TitleBgActive]          = ImVec4(0.27f, 0.09f, 0.37f, 1.00f);
+    colors[ImGuiCol_CheckMark]              = ImVec4(0.27f, 0.27f, 0.76f, 1.00f);
+    colors[ImGuiCol_SliderGrab]             = ImVec4(0.19f, 0.09f, 0.47f, 1.00f);
+    colors[ImGuiCol_SliderGrabActive]       = ImVec4(0.62f, 0.42f, 0.65f, 1.00f);
+    colors[ImGuiCol_Button]                 = ImVec4(0.27f, 0.27f, 0.76f, 0.40f);
+    colors[ImGuiCol_ButtonHovered]          = ImVec4(0.50f, 0.40f, 0.79f, 1.00f);
+    colors[ImGuiCol_ButtonActive]           = ImVec4(0.62f, 0.42f, 0.65f, 1.00f);
+    colors[ImGuiCol_Header]                 = ImVec4(0.27f, 0.27f, 0.76f, 0.31f);
+    colors[ImGuiCol_HeaderHovered]          = ImVec4(0.47f, 0.27f, 0.76f, 0.80f);
+    colors[ImGuiCol_HeaderActive]           = ImVec4(0.62f, 0.42f, 0.65f, 1.00f);
+    colors[ImGuiCol_Separator]              = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
+    colors[ImGuiCol_SeparatorHovered]       = ImVec4(0.62f, 0.42f, 0.65f, 0.78f);
+    colors[ImGuiCol_SeparatorActive]        = ImVec4(0.62f, 0.42f, 0.65f, 1.00f);
+    colors[ImGuiCol_ResizeGrip]             = ImVec4(0.62f, 0.42f, 0.65f, 0.20f);
+    colors[ImGuiCol_ResizeGripHovered]      = ImVec4(0.62f, 0.42f, 0.65f, 0.67f);
+    colors[ImGuiCol_ResizeGripActive]       = ImVec4(0.62f, 0.42f, 0.65f, 0.95f);
+    colors[ImGuiCol_Tab]                    = ImVec4(0.35f, 0.16f, 0.45f, 0.86f);
+    colors[ImGuiCol_TabHovered]             = ImVec4(0.62f, 0.42f, 0.65f, 1.00f);
+    colors[ImGuiCol_TabActive]              = ImVec4(0.67f, 0.33f, 0.82f, 1.00f);
+    colors[ImGuiCol_TabUnfocused]           = ImVec4(0.11f, 0.07f, 0.15f, 0.97f);
+    colors[ImGuiCol_TabUnfocusedActive]     = ImVec4(0.30f, 0.14f, 0.42f, 1.00f);
+    colors[ImGuiCol_DockingPreview]         = ImVec4(0.62f, 0.42f, 0.65f, 0.70f);
 
     Engine::getWindow()->setBackgroundColor(ColorRGB{0.15f});
 
