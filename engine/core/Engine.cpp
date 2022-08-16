275ce79dd01f93dd3128814e4fd9245f7e964b3d
ash <anticream39@yahoo.com>
2022-08-15 21:10:31 -0400

    how the hell did I forget a ;

--- engine/core/Engine.cpp
+++ engine/core/Engine.cpp
@@ -248,7 +248,7 @@ std::string Engine::getConfigDir() {
 #else // We must be on windows
     // Get the environment vairable for the user on windows
     pathString = getenv("USERPROFILE");
-    pathString.append("\\Application Data\\Roaming\\ChiraEngine\\")
+    pathString.append("\\Application Data\\Roaming\\ChiraEngine\\");
 #endif
     return pathString;
 }
