c8fc1d7f757746c6b3bc3c7f2683553ee54e9e3e
craftablescience <me@craftablescience.info>
2022-11-02 12:04:07 -0400

    fix: globals that used to be in Config are not now

--- engine/entity/root/Window.cpp
+++ engine/entity/root/Window.cpp
@@ -5,6 +5,7 @@
 #include <fmt/core.h>
 #include <glad/glversion.h>
 
+#include <config/Config.h>
 #include <config/ConEntry.h>
 #include <core/Engine.h>
 #include <event/Events.h>
@@ -53,8 +54,8 @@ static ConVar win_vsync{"win_vsync", true, "Limit the FPS to your monitor's reso
 static ConVar input_raw_mouse_motion{"input_raw_mouse_motion", true, "Get more accurate mouse motion.", CON_FLAG_CACHE}; // NOLINT(cert-err58-cpp)
 
 bool Window::createGLFWWindow(std::string_view title) {
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, Config::GL_VERSION_MAJOR);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, Config::GL_VERSION_MINOR);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_VERSION_MAJOR);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_VERSION_MINOR);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 #ifdef DEBUG
@@ -77,7 +78,7 @@ bool Window::createGLFWWindow(std::string_view title) {
     glfwMakeContextCurrent(this->window);
 
     if (!gladLoadGL(glfwGetProcAddress)) {
-        LOG_WINDOW.error(fmt::format("OpenGL {} must be available to run this program", Config::GL_VERSION_STRING_PRETTY));
+        LOG_WINDOW.error(fmt::format("OpenGL {} must be available to run this program", GL_VERSION_STRING_PRETTY));
         return false;
     }
     glfwSwapInterval(win_vsync.getValue<int>());
@@ -153,10 +154,10 @@ bool Window::createGLFWWindow(std::string_view title) {
     ImGui::SetCurrentContext(this->imguiContext);
     auto& io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard | ImGuiConfigFlags_NavEnableGamepad | ImGuiConfigFlags_DockingEnable;
-    this->setImGuiConfigPath();
+    Window::setImGuiConfigPath();
 
     ImGui_ImplGlfw_InitForOpenGL(this->window, true); // register for default input binds
-    ImGui_ImplOpenGL3_Init(Config::GL_VERSION_STRING.data());
+    ImGui_ImplOpenGL3_Init(GL_VERSION_STRING.data());
 
     auto defaultFont = Resource::getUniqueResource<Font>("file://fonts/default.json");
     ImGui::GetIO().FontDefault = defaultFont->getFont();
@@ -191,7 +192,7 @@ void Window::render(glm::mat4 /*parentTransform*/) {
     glfwMakeContextCurrent(this->window);
     ImGui::SetCurrentContext(this->imguiContext);
 
-    this->setImGuiConfigPath();
+    Window::setImGuiConfigPath();
 
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
