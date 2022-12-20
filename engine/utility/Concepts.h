05ef811ecdefec83a85919e1cf0efd8c30262c90
craftablescience <me@craftablescience.info>
2022-12-19 22:45:27 -0500

    fix: angelscript binding function for functions with no parameters or primitive pointers

---
+++ engine/utility/Concepts.h
@@ -0,0 +1,12 @@
+#include <concepts>
+#include <type_traits>
+
+namespace chira {
+
+template<typename T>
+concept CArithmetic = std::is_arithmetic_v<T>;
+
+template<typename T>
+concept CNonArithmetic = !std::is_arithmetic_v<T>;
+
+} // namespace chira
