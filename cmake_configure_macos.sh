51bd97b5aa50272938bfd9a5e22f509590697d73
ashifolfi <anticream39@yahoo.com>
2022-12-22 16:29:19 -0500

    fix: double quote variables in shell scripts

--- cmake_configure_macos.sh
+++ cmake_configure_macos.sh
@@ -20,14 +20,14 @@ config_xcode()
 
 check_param()
 {
-    TYPE=$1
+    TYPE="$1"
     
-    if [ $TYPE ];
+    if [ "$TYPE" ];
     then
-        if [ $TYPE == "Xcode" ];
+        if [ "$TYPE" == "Xcode" ];
         then
             config_xcode
-        elif [ $TYPE == "Makefiles" ];
+        elif [ "$TYPE" == "Makefiles" ];
         then
             config_noxcode
         else
@@ -40,4 +40,4 @@ check_param()
     fi
 }
 
-check_param $1
+check_param "$1"
