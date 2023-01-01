add764689af0ed999c1c64559c7e62487daf4fab
ashifolfi <anticream39@yahoo.com>
2023-01-01 16:42:02 -0500

    Add base CS bindings

---
+++ engine/script/Sharp/ChiraSharp/ChiraSharp/Core/CE.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ChiraSharp {
+    public class CE
+    {
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static public void Print(string message);
+    }
+}
