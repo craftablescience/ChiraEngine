884382443548eea322adaea3fa20168070264aa1
ashifolfi <anticream39@yahoo.com>
2023-01-01 12:34:05 -0500

    section AS into it's own folder and add Mono files

---
+++ engine/script/Sharp/MonoRuntime.cpp
@@ -0,0 +1,33 @@
+#include "MonoRuntime.h"
+
+#include <core/Logger.h>
+#include <i18n/TranslationManager.h>
+
+using namespace chira;
+
+CHIRA_CREATE_LOG(MONORUNTIME);
+
+static void print(MonoString* message);
+
+void MonoRuntime::init() {
+    // mono_config_parse (NULL);
+    MonoRuntime::domain = mono_jit_init ("ChiraEngineMain");
+
+    if (MonoRuntime::domain == nullptr) {
+        throw std::runtime_error(TRC("error.monoruntime.jit_init_failure"));
+    }
+
+    // register some base calls for testing
+    mono_add_internal_call("CE::LogInfo", (const void*)print);
+
+    // load in the base assembly
+    MonoRuntime::ceassembly = mono_domain_assembly_open(MonoRuntime::domain, "ChiraSharp.dll");
+}
+
+void MonoRuntime::shutdown() {
+    mono_jit_cleanup(MonoRuntime::domain);
+}
+
+static void print(MonoString* message) {
+    LOG_MONORUNTIME.info(mono_string_to_utf8(message));
+}
