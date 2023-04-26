427c571c4b381b29b394b18cd5755eb8a8809fdd
K J <anticream39@yahoo.com>
2023-04-26 14:45:21 +0000

    include all components in inspector panel for testing

--- editor/ui/InspectorPanel.h
+++ editor/ui/InspectorPanel.h
@@ -2,6 +2,23 @@
 
 #include <entity/Entity.h>
 #include <ui/IPanel.h>
+#include <imfilebrowser.h>
+
+#include <entity/component/AngelScriptComponent.h>
+#include <entity/component/AudioNoiseComponent.h>
+#include <entity/component/AudioSfxrComponent.h>
+#include <entity/component/AudioSpeechComponent.h>
+#include <entity/component/AudioWavComponent.h>
+#include <entity/component/AudioWavStreamComponent.h>
+#include <entity/component/CameraComponent.h>
+#include <entity/component/LightComponents.h>
+#include <entity/component/MeshComponent.h>
+#include <entity/component/MeshDynamicComponent.h>
+#include <entity/component/NameComponent.h>
+#include <entity/component/SkyboxComponent.h>
+#include <entity/component/TagComponents.h>
+#include <entity/component/TransformComponent.h>
+#include <entity/component/UUIDComponent.h>
 
 namespace chira {
 
