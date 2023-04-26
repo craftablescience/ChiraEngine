66fd0ab5a6386cd453c96d2eb7c050b7efb715ee
craftablescience <me@craftablescience.info>
2023-04-26 19:03:53 -0400

    fix: pop id before breaking out of loop

--- editor/ui/EntitySelectPanel.cpp
+++ editor/ui/EntitySelectPanel.cpp
@@ -19,14 +19,13 @@ void EntitySelectPanel::renderContents() {
         ImGui::PushID(++id);
         if (ImGui::CollapsingHeader(scene->getName().c_str(), ImGuiTreeNodeFlags_DefaultOpen)) {
             for (const auto& [entityID, entity] : scene->getEntities()) {
-                std::string buttonid = "X##";
-                buttonid += entity->getName();
-                if (ImGui::Button(buttonid.c_str())) {
+                ImGui::PushID(++id);
+                if (ImGui::Button("X")) {
                     scene->removeEntity(entityID);
+                    ImGui::PopID();
                     break;
                 }
                 ImGui::SameLine();
-                ImGui::PushID(++id);
                 if (ImGui::Button(entity->getName().c_str())) {
                     this->editor->setSelected(entity.get());
                     this->inspector->setSelected(entity.get());
