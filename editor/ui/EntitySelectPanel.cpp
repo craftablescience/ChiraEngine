ee895120ec054849dcdfedb21fac5c30f1492c28
craftablescience <me@craftablescience.info>
2023-04-26 14:58:23 -0400

    feat: add entity and scene creation fields

--- editor/ui/EntitySelectPanel.cpp
+++ editor/ui/EntitySelectPanel.cpp
@@ -11,14 +11,32 @@ EntitySelectPanel::EntitySelectPanel(Layer* layer_, InspectorPanel* inspector_)
         , inspector(inspector_) {}
 
 void EntitySelectPanel::renderContents() {
+    int id = 0;
+
     for (const auto& [sceneID, scene] : this->layer->getScenes()) {
+        ImGui::PushID(++id);
         if (ImGui::CollapsingHeader(scene->getName().c_str(), ImGuiTreeNodeFlags_DefaultOpen)) {
             for (const auto& [entityID, entity] : scene->getEntities()) {
+                ImGui::PushID(++id);
                 if (ImGui::Button(entity->getName().c_str())) {
                     this->inspector->setEntity(entity.get());
                 }
+                ImGui::PopID();
+            }
+
+            char entityName[512] {0};
+            if (ImGui::InputText("Add Entity", entityName, sizeof(entityName), ImGuiInputTextFlags_EnterReturnsTrue) && entityName[0]) {
+                scene->addEntity(entityName);
             }
         }
+        ImGui::PopID();
+    }
+
+    ImGui::Separator();
+
+    char sceneName[512] {0};
+    if (ImGui::InputText("Add Scene", sceneName, sizeof(sceneName), ImGuiInputTextFlags_EnterReturnsTrue) && sceneName[0]) {
+        this->layer->addScene(sceneName);
     }
 }
 
