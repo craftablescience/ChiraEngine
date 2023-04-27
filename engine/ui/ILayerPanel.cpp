6f113e789bd7dcf2a4d2089a7d144f7995db59e8
craftablescience <me@craftablescience.info>
2023-04-27 00:46:38 -0400

    feat: in layer panels, don't resize layer if requested not to

--- engine/ui/ILayerPanel.cpp
+++ engine/ui/ILayerPanel.cpp
@@ -4,17 +4,20 @@
 
 using namespace chira;
 
-ILayerPanel::ILayerPanel(const std::string& title_, Layer* layer_, bool startVisible, ImVec2 windowSize, bool enforceSize)
+ILayerPanel::ILayerPanel(const std::string& title_, Layer* layer_, bool resizeLayer_, bool startVisible, ImVec2 windowSize, bool enforceSize)
     : IPanel(title_, startVisible, windowSize, enforceSize)
     , layer(layer_)
-    , currentSize(windowSize.x, windowSize.y) {}
+    , currentSize(windowSize.x, windowSize.y)
+    , shouldResize(resizeLayer_) {}
 
 void ILayerPanel::renderContents() {
     if (ImGui::BeginChild("__internal_frame__")) {
         ImVec2 guiSize = ImGui::GetWindowSize();
         glm::vec2i size{guiSize.x, guiSize.y};
         if (this->currentSize != size) {
-            this->layer->setSize(size);
+            if (this->shouldResize) {
+                this->layer->setSize(size);
+            }
             this->currentSize = size;
         }
         ImGui::Image(Renderer::getImGuiFrameBufferHandle(*this->layer->getRawHandle()), guiSize, ImVec2(0, 1), ImVec2(1, 0));
