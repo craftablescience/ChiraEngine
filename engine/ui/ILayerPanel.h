329bf8098e2edaaf570317b717a476cea115ffe9
craftablescience <me@craftablescience.info>
2023-04-27 00:46:38 -0400

    feat: in layer panels, don't resize layer if requested not to

--- engine/ui/ILayerPanel.h
+++ engine/ui/ILayerPanel.h
@@ -9,12 +9,13 @@ class Layer;
 
 class ILayerPanel : public IPanel {
 public:
-    ILayerPanel(const std::string& title_, Layer* layer_, bool startVisible, ImVec2 windowSize = {}, bool enforceSize = false);
+    ILayerPanel(const std::string& title_, Layer* layer_, bool resizeLayer, bool startVisible, ImVec2 windowSize = {}, bool enforceSize = false);
     void renderContents() final;
     virtual void renderLayerContents() = 0;
 protected:
     Layer* layer;
     glm::vec2i currentSize;
+    bool shouldResize;
 };
 
 } // namespace chira
