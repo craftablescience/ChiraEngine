d350f99b002f3ba0e0bf4cad7dbeb2c129fc0a50
craftablescience <me@craftablescience.info>
2023-05-26 22:07:57 -0400

    chore: remove swapEndian function as c++20 obsoletes it

--- engine/core/Platform.h
+++ engine/core/Platform.h
@@ -50,16 +50,4 @@ namespace chira {
     #error "A CHIRA_XXBIT macro must be set!"
 #endif
 
-/// Changes the endianness of a type. Should only be used with numbers.
-template<typename T> inline T swapEndian(T t) {
-    union {
-        T t;
-        byte u8[sizeof(T)];
-    } source{}, dest{};
-    source.t = t;
-    for (size_t k = 0; k < sizeof(T); k++)
-        dest.u8[k] = source.u8[sizeof(T) - k - 1];
-    return dest.t;
-}
-
 } // namespace chira
