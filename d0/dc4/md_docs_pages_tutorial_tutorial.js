736a0f0536f1c2c8365a9239a8d6eb00f07d5a3c
craftablescience <brendanelewis@gmail.com>
2023-05-30 19:11:30 +0000

    Deploying to docs from @ craftablescience/ChiraEngine@08ae2576c3de138973022619f532cf65b64e1d69 🚀

--- d0/dc4/md_docs_pages_tutorial_tutorial.js
+++ d0/dc4/md_docs_pages_tutorial_tutorial.js
@@ -1,11 +1,11 @@
 var md_docs_pages_tutorial_tutorial =
 [
     [ "Creating a Chira Engine Project", "dd/daa/createchiraengproj.html", [
-      [ "Prerequisites:", "dd/daa/createchiraengproj.html#autotoc_md26", [
-        [ "macOS Dependencies:", "dd/daa/createchiraengproj.html#autotoc_md27", null ],
-        [ "Linux Dependencies", "dd/daa/createchiraengproj.html#autotoc_md28", null ],
-        [ "Windows Dependencies", "dd/daa/createchiraengproj.html#autotoc_md29", null ]
+      [ "Prerequisites:", "dd/daa/createchiraengproj.html#autotoc_md27", [
+        [ "macOS Dependencies:", "dd/daa/createchiraengproj.html#autotoc_md28", null ],
+        [ "Linux Dependencies", "dd/daa/createchiraengproj.html#autotoc_md29", null ],
+        [ "Windows Dependencies", "dd/daa/createchiraengproj.html#autotoc_md30", null ]
       ] ],
-      [ "Creating the base project", "dd/daa/createchiraengproj.html#autotoc_md30", null ]
+      [ "Creating the base project", "dd/daa/createchiraengproj.html#autotoc_md31", null ]
     ] ]
 ];
--- dd/db2/md_docs_pages_filespec_filespec.js
+++ dd/db2/md_docs_pages_filespec_filespec.js
@@ -1,32 +1,34 @@
 var md_docs_pages_filespec_filespec =
 [
     [ "Features", "index.html#autotoc_md0", null ],
-    [ "Supported Platforms", "index.html#autotoc_md1", null ],
-    [ "Supported Compilers", "index.html#autotoc_md2", null ],
-    [ "Bundled Dependencies", "index.html#autotoc_md3", null ],
-    [ "Development (Windows)", "index.html#autotoc_md4", null ],
-    [ "Development (Linux)", "index.html#autotoc_md5", null ],
-    [ "Development (macOS)", "index.html#autotoc_md6", null ],
-    [ "Usage", "index.html#autotoc_md7", null ],
-    [ "Common info", "dd/db2/md_docs_pages_filespec_filespec.html#autotoc_md9", null ],
-    [ "Formats", "dd/db2/md_docs_pages_filespec_filespec.html#autotoc_md10", null ],
-    [ "Tools", "dd/db2/md_docs_pages_filespec_filespec.html#autotoc_md11", null ],
+    [ "Supported Platforms", "index.html#autotoc_md1", [
+      [ "Planned:", "index.html#autotoc_md2", null ]
+    ] ],
+    [ "Supported Compilers", "index.html#autotoc_md3", null ],
+    [ "Supported Rendering APIs", "index.html#autotoc_md4", null ],
+    [ "Bundled Dependencies", "index.html#autotoc_md5", null ],
+    [ "Development (Windows)", "index.html#autotoc_md6", null ],
+    [ "Development (Linux)", "index.html#autotoc_md7", null ],
+    [ "Development (macOS)", "index.html#autotoc_md8", null ],
+    [ "Common info", "dd/db2/md_docs_pages_filespec_filespec.html#autotoc_md10", null ],
+    [ "Formats", "dd/db2/md_docs_pages_filespec_filespec.html#autotoc_md11", null ],
+    [ "Tools", "dd/db2/md_docs_pages_filespec_filespec.html#autotoc_md12", null ],
     [ "CSCN Files", "d4/dd3/cscnspec.html", [
-      [ "Format", "d4/dd3/cscnspec.html#autotoc_md19", null ],
-      [ "Example scene file", "d4/dd3/cscnspec.html#autotoc_md20", null ],
-      [ "SceneInfo", "d4/dd3/cscnspec.html#autotoc_md21", null ],
-      [ "Entities", "d4/dd3/cscnspec.html#autotoc_md22", null ]
+      [ "Format", "d4/dd3/cscnspec.html#autotoc_md20", null ],
+      [ "Example scene file", "d4/dd3/cscnspec.html#autotoc_md21", null ],
+      [ "SceneInfo", "d4/dd3/cscnspec.html#autotoc_md22", null ],
+      [ "Entities", "d4/dd3/cscnspec.html#autotoc_md23", null ]
     ] ],
     [ "CMDL Files", "d4/def/cmdlspec.html", [
-      [ "Format", "d4/def/cmdlspec.html#autotoc_md12", null ],
-      [ "Properties", "d4/def/cmdlspec.html#autotoc_md13", [
-        [ "Material Type", "d4/def/cmdlspec.html#autotoc_md14", null ],
-        [ "Material", "d4/def/cmdlspec.html#autotoc_md15", null ],
-        [ "Model", "d4/def/cmdlspec.html#autotoc_md16", null ],
-        [ "Loader", "d4/def/cmdlspec.html#autotoc_md17", null ]
+      [ "Format", "d4/def/cmdlspec.html#autotoc_md13", null ],
+      [ "Properties", "d4/def/cmdlspec.html#autotoc_md14", [
+        [ "Material Type", "d4/def/cmdlspec.html#autotoc_md15", null ],
+        [ "Material", "d4/def/cmdlspec.html#autotoc_md16", null ],
+        [ "Model", "d4/def/cmdlspec.html#autotoc_md17", null ],
+        [ "Loader", "d4/def/cmdlspec.html#autotoc_md18", null ]
       ] ]
     ] ],
     [ "CMSH Files", "dc/d10/cmshspec.html", [
-      [ "Format", "dc/d10/cmshspec.html#autotoc_md18", null ]
+      [ "Format", "dc/d10/cmshspec.html#autotoc_md19", null ]
     ] ]
 ];
