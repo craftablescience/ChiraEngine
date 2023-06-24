6f1848e17967281e4dd705f39cabbefbb62ea36e
craftablescience <me@craftablescience.info>
2023-06-24 00:17:32 -0400

    feat: add executable icon and manifest to tools on windows

--- tools/editor/editor.cmake
+++ tools/editor/editor.cmake
@@ -1,15 +1,12 @@
 include(${CMAKE_CURRENT_LIST_DIR}/ui/CMakeLists.txt)
 list(APPEND CHIRA_EDITOR_SOURCES
-        ${CMAKE_CURRENT_LIST_DIR}/Editor.cpp)
+        ${CMAKE_CURRENT_LIST_DIR}/editor.cpp)
 
-if(WIN32)
-    list(APPEND CHIRA_EDITOR_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/resources/editor/platform/windows/app.rc)
-endif()
 if(APPLE)
-    list(APPEND CHIRA_APPLE_BUNDLE_FILES
+    list(APPEND CHIRA_EDITOR_APPLE_BUNDLE_FILES
             ${CMAKE_CURRENT_SOURCE_DIR}/resources/editor/platform/macOS/AppIcon.icns
             ${CMAKE_CURRENT_SOURCE_DIR}/resources/editor/platform/macOS/Info.plist)
-    list(APPEND CHIRA_EDITOR_SOURCES ${CHIRA_APPLE_BUNDLE_FILES})
+    list(APPEND CHIRA_EDITOR_SOURCES ${CHIRA_EDITOR_APPLE_BUNDLE_FILES})
 endif()
 
 set(CHIRA_EDITOR_NAME "editor")
@@ -19,7 +16,5 @@ if(APPLE)
     set_target_properties(${CHIRA_EDITOR_NAME} PROPERTIES
             MACOSX_BUNDLE ON
             MACOSX_FRAMEWORK_IDENTIFIER info.craftablescience.ChiraEngine
-            BUNDLE "${CHIRA_APPLE_BUNDLE_FILES}")
+            BUNDLE "${CHIRA_EDITOR_APPLE_BUNDLE_FILES}")
 endif()
-target_link_libraries(${CHIRA_EDITOR_NAME} PRIVATE ${CHIRA_ENGINE_NAME})
-target_include_directories(${CHIRA_EDITOR_NAME} PRIVATE .)
