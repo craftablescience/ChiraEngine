5d44602ce3b1af396414102b24abc8f7915d5ee4
K J <anticream39@yahoo.com>
2023-08-16 22:06:40 -0400

    refactor: better index values for layers
    Co-authored-by: Laura <me@craftablescience.info>

--- engine/entity/component/LayerComponents.h
+++ engine/entity/component/LayerComponents.h
@@ -2,15 +2,15 @@
 
 namespace chira {
 
-struct Layer0Component { inline static int index = 1; };
-struct Layer1Component { inline static int index = 2; };
-struct Layer2Component { inline static int index = 4; };
-struct Layer3Component { inline static int index = 8; };
-struct Layer4Component { inline static int index = 16; };
-struct Layer5Component { inline static int index = 32; };
-struct Layer6Component { inline static int index = 64; };
-struct Layer7Component { inline static int index = 128; };
-struct Layer8Component { inline static int index = 256; };
-struct Layer9Component { inline static int index = 512; };
+struct Layer0Component { inline static int index = 1 << 0; };
+struct Layer1Component { inline static int index = 1 << 1; };
+struct Layer2Component { inline static int index = 1 << 2; };
+struct Layer3Component { inline static int index = 1 << 3; };
+struct Layer4Component { inline static int index = 1 << 4; };
+struct Layer5Component { inline static int index = 1 << 5; };
+struct Layer6Component { inline static int index = 1 << 6; };
+struct Layer7Component { inline static int index = 1 << 7; };
+struct Layer8Component { inline static int index = 1 << 8; };
+struct Layer9Component { inline static int index = 1 << 9; };
 
 } // namespace chira
