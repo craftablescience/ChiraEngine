7bad76e6b7c1b40e6a8d48f43db799b91cd15bdf
craftablescience <me@craftablescience.info>
2023-11-12 16:55:05 -0500

    feat: print_variables cmake macro

--- cmake/PrintVariable.cmake
+++ cmake/PrintVariable.cmake
@@ -3,4 +3,9 @@ macro(print_variable VARIABLE)
     message(STATUS "  ${VARIABLE}: ${${VARIABLE}}")
 endmacro()
 
-#todo(wonder): print_variables
+# Helper to print several variables in a row
+macro(print_variables)
+    foreach(VARIABLE IN ITEMS ${ARGN})
+        print_variable(${VARIABLE})
+    endforeach()
+endmacro()
